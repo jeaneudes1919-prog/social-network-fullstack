5cfce143cab03ff4f8c513dba8683925fe68c20c
jeaneudes1919-prog <jeaneudesatindehou@gmail.com>
2025-12-10 12:02:01 +0100

    Modification cloudinary

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -3,20 +3,18 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 require('dotenv').config();
 
-// 1. Connexion à ton compte
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-// 2. Configuration du stockage
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'devsocial', // Le nom du dossier dans ton Cloudinary
-    allowed_formats: ['jpg', 'png', 'jpeg', 'gif', 'webp'], // Formats acceptés
-    transformation: [{ width: 1000, height: 1000, crop: 'limit' }] // Optionnel : redimensionne les images géantes
+    folder: 'devsocial',
+    resource_type: 'auto', // Laisse Cloudinary décider (image/video)
+    // On enlève 'allowed_formats' pour éviter les bugs de formats stricts
   }
 });
 
